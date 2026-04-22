afe4d9a07998f9850a8496fec1901c541838647a
54ids <2207103764@qq.com>
2026-04-22 23:27:42 +0800

    first-commit

---
+++ my_main.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/opencv.hpp>
+#include "my_process.h"
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+	my_process qt;
+	qt.blackground_init( );
+	return 0;
+}
